2877d4dd059c8667c9b80e06a8d5090aaf28bf73
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-11-28 16:40:32 +0700

    create user repo

---
+++ src/main/java/event/project/users/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package event.project.users.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import event.project.users.models.User;
+
+public interface UserRepository extends JpaRepository<User, String>{
+  
+}
