235a9b68acd7ecf7b4981ebf7eca35f5e0e11dbf
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-11-29 19:14:35 +0700

    person structure

---
+++ src/main/java/event/project/person/services/PersonService.java
@@ -0,0 +1,11 @@
+package event.project.person.services;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class PersonService {
+  
+}
